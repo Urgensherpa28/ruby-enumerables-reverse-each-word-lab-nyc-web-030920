c6901e6edf5aee964b4222931520f770a59dabf9
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-13 22:30:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
-  new = ""
   array = string.split
-  # array.each do |word| 
-  # word.reverse 
-  # end 
+  array.each do |word|
+    p word.reverse 
+  end 
   array 
 end
